d10a3756397d63920929d84a624d2fd16b87e864
Timothée <segard.timothee@gmail.com>
2020-05-01 19:38:46 +0200

    Articles - creation and delete methods when admin is loged

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -15,6 +15,14 @@ use Twig\Error\SyntaxError;
 class ArticlesController extends MainController
 {
 
+
+    /**
+     * @return array
+     */
+    private $post_content = [];
+
+
+
     /**
      * Renders the View Home
      * @return string
@@ -43,9 +51,67 @@ class ArticlesController extends MainController
         ]);
     }
 
+    /**
+     * @return string
+     */
+    private function postData()
+    {
+        $this->post_content['title'] = $this->post['title'];
+        $this->post_content['content'] = $this->post['content'];
+    }
 
 
 
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+        $title   = $this->post['title'];
+        $content = $this->post['content'];
+        if (empty($title && $content)) {
+            $this->redirect('admin');
+        }
+        $createdArticle = ModelFactory::getModel('Articles')->createIt($title,$content);
+        $this->redirect('admin', ['createdArticle' => $createdArticle]);
+    }
 
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function deleteMethod()
+    {
+        $id_article = $this->get['id'];
+
+        $article_confirmed = ModelFactory::getModel('Comments')->listData($id_article, 'article_id');
+
+        if (!empty($article_confirmed))
+        {
+            ModelFactory::getModel('Comments')->deleteData($id_article, 'article_id');
+        }
+
+        ModelFactory::getModel('Articles')->deleteData($id_article);
+
+        $this->redirect('admin');
+    }
+
+
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function modifyMethod()
+    {
+
+    }
 
 }
