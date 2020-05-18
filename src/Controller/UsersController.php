5601f23236663dac2f09f51ad5fb4c0222134c41
Timothée <segard.timothee@gmail.com>
2020-05-18 10:09:59 +0200

    avoid connect for admin

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -89,15 +89,29 @@ class UsersController extends MainController
             else echo 'adresse ou mot de passe invalide';
         }
 
-        //avoid password typing for admin when session is still open
 
-        elseif ($this->session['user']['admin'] === '1'){
-
-            $this->redirect('admin');
-        }
 
         return $this->twig->render('login.twig');
     }
+
+
+
+    public function adminredirectMethod()
+    {
+
+
+        //avoid password typing for admin when session is still open
+
+        if ($this->session['user']['admin'] === '1') {
+            $this->redirect('admin');
+        } elseif ($this->session['user']['admin'] === '0') {
+            $this->redirect('adminUser');
+        }
+        $this->redirect('home');
+
+
+    }
+
 
 
 
